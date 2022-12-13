84a38f591e531b0e21a2e0f11b1c0f5bd4f7df81
Foteraz <Foteraz@users.noreply.github.com>
2022-12-13 22:56:17 +0700

    Version 2.1

---
+++ assets/js/FinproJS.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+      $("#box").slideToggle(1000);
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#button2").click(function(){
+      $("#box2").slideToggle(1000);
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#button3").click(function(){
+      $("#box3").slideToggle(1000);
+    });
+  });
+  
+        $(document).ready(function(){
+          $("#button4").click(function(){
+            $("#box4").slideToggle(1000);
+          });
+        });
+        
+        $(document).ready(function(){
+          $("#button5").click(function(){
+            $("#box5").slideToggle(1000);
+          });
+        });
